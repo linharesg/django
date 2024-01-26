b30cbcc3b861c3d3128bb6fb4ed3cebcab8cedf0
Gabriel Linhares <linhares.gs96@gmail.com>
2024-01-26 00:17:19 -0300

    remove STATICFILES_DIRS

--- aula_02/inventory_control/inventory_control/settings.py
+++ aula_02/inventory_control/inventory_control/settings.py
@@ -124,9 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'inventory_control'/ 'base' / 'staticfiles'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'base' / 'staticfiles'
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
