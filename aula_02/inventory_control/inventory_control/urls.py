508cd4d8c127e80578f294981407fd0e4280c5b8
Gabriel Linhares <linhares.gs96@gmail.com>
2024-01-31 21:45:04 -0300

    feat: add login/logout system

--- aula_02/inventory_control/inventory_control/urls.py
+++ aula_02/inventory_control/inventory_control/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('fornecedores/', include('suppliers.urls')),
     path('produtos/', include('products.urls')),
+    path('', include('users.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
