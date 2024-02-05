624d0444ec89567fb194abfa2f5eed1f7e20878e
Gabriel Linhares <linhares.gs96@gmail.com>
2024-02-05 20:02:18 -0300

    refactor: replace suppliers views by generic views templates from django

--- aula_02/inventory_control/suppliers/urls.py
+++ aula_02/inventory_control/suppliers/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
 from . import views
-
+from .views import SupplierListView, SupplierSearchView, SupplierCreateView, SupplierUpdateView, SupplierDeleteView
 app_name = "suppliers"
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("cadastro", views.create, name="create"),
-    path("<int:id>/delete", views.delete, name="delete"),
-    path("search", views.search, name="search"),
+    # path("", views.index, name="index"),
+    path("", SupplierListView.as_view(), name="index"),
+    # path("cadastro/", views.create, name="create"),
+    path("cadastro/", SupplierCreateView.as_view(), name="create"),
+    # path("<int:id>/delete", views.delete, name="delete"),
+    path("<int:pk>/delete", SupplierDeleteView.as_view(), name="delete"),
+    # path("search", views.search, name="search"),
+    path("search", SupplierSearchView.as_view(), name="search"),
     path("<int:id>/toggle_enabled", views.toggle_enabled, name="toggle_enabled"),
-    path("<slug:slug>/", views.update, name="update"),
+    # path("<slug:slug>/", views.update, name="update"),
+    path("<slug:slug>/", views.SupplierUpdateView.as_view(), name="update"),
 ]
--- aula_02/inventory_control/suppliers/views.py
+++ aula_02/inventory_control/suppliers/views.py
@@ -6,71 +6,66 @@ from django.http import JsonResponse
 from django.contrib import messages
 from .models import Supplier
 from .forms import SupplierForm
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView,  DetailView, CreateView, UpdateView, DeleteView
 
-def index(request):
-    suppliers = Supplier.objects.order_by("-id")
+class SupplierListView(ListView):
+    model = Supplier
+    template_name = "suppliers/index.html"
+    paginate_by = 1
+    ordering = "-id"
+    # Escrito dessa forma, substitui a view "index" que tinha sido criada antes
     
-    paginator = Paginator(suppliers, 2)
-    #/fornecedores?page=1
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
+class SupplierSearchView(ListView):
+    model = Supplier
+    template_name = "suppliers/index.html"
+    paginate_by = 1
+    # ordering = "-id" Ordenação deve ser feita direto no queryset pois houve sobreescrita de queryset
 
-    context = { "suppliers": page_obj }
-    return render(request, "suppliers/index.html", context)
-
-def search(request):
-    # Obtendo o valor da requisição (Formulário)
-    search_value = request.GET.get("q").strip()
-
-    # Verificando se algo foi digitado
-    if not search_value:
-        return redirect("suppliers:index")
-    
-    #Filtrando os fornecedores
-    # suppliers = Supplier.objects.filter(fantasy_name__icontains=search_value)
-
-    # O "Q" é usado para combinar filtros (AND (&) ou | (OR))
-    suppliers = Supplier.objects \
-        .filter(Q(fantasy_name__icontains=search_value) | 
-            Q(company_name__icontains=search_value)) \
-        .order_by("-id")
-
-    paginator = Paginator(suppliers, 2)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-
-    context = { "suppliers": page_obj}
-
-    return render(request, "suppliers/index.html", context)
-
-def create(request):
-    
-    form_action = reverse("suppliers:create")
-    
-    if request.method == "POST":
-        form = SupplierForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-            
-            messages.success(request, "O fornecedor foi cadastrado com sucesso!")
-            
-            return redirect ("suppliers:index")
-        # print(form.errors)
+    def get_queryset(self):
+        search_value = self.request.GET.get("q").strip()
         
-        messages.error(request, "Falha ao cadastrar o fornecedor. Verifique o preenchimento dos campos.")
+        if not search_value:
+            return Supplier.objects.all().order_by("-id")
+       
+        return Supplier.objects.filter(
+                                    Q(fantasy_name__icontains=search_value) | 
+                                    Q(company_name__icontains=search_value)).order_by("-id")
+
+class SupplierCreateView(CreateView):
+    model = Supplier
+    template_name = "suppliers/create.html"
+    form_class = SupplierForm
+    success_url = reverse_lazy("suppliers:index")
+    
+    def form_valid(self, form):
         
-        context = { "form": form, "form_action": form_action }
-        
-        return render(request, "suppliers/create.html", context)
+        # Save no banco de dados colocado antes da mensage, porque caso dê algum erro na hora de salvar, vai esoturar exceção e não vai mostrar a mensagem de sucesso
+        response = super().form_valid(form)
+        messages.success(self.request, "Fornecedor cadastrado com sucesso!")
+        return response
     
-    #GET
-    form = SupplierForm()
-    
-    context = { "form": form, "form_action": form_action }
-    
-    return render(request, "suppliers/create.html", context)
+    def get_context_date(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form_action"] = reverse("suppliers:create")
+        return context
+
+
+class SupplierUpdateView(UpdateView):
+    model = Supplier
+    template_name = "suppliers/update.html"
+    form_class = SupplierForm
+    success_url = reverse_lazy("suppliers:index")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, "Fornecedor atualizado com sucesso!")
+        return response
+
+    def form_invalid(self, form):
+        response = super().form_invalid(form)
+        messages.success(self.request, "Erro ao atualizar o fornecedor!")
+        return response
 
 def update(request, slug):
     supplier = get_object_or_404(Supplier, slug=slug)
@@ -103,14 +98,11 @@ def update(request, slug):
 
     return render(request, "suppliers/create.html", context)
 
+class SupplierDeleteView(DeleteView):
+    model = Supplier
+    success_url = reverse_lazy("suppliers:index")
+
 # só permite que a view seja acessada através de POST, e não de GET
-@require_POST
-def delete(request, id):
-    supplier = get_object_or_404(Supplier, pk=id)
-    supplier.delete()
-
-    return redirect("suppliers:index")
-
 @require_POST
 # def toggle_enabled(request, id):
 def toggle_enabled(request, id):
